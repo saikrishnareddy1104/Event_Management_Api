5bb153c1373509ddb13e7762b4e9e66d74dfa05a
saikrishnareddy1104 <saikrishnareddy.singi@gmail.com>
2021-01-23 15:05:53 +0530

    url change

--- event_api_app/urls.py
+++ event_api_app/urls.py
@@ -7,8 +7,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('event/',event_list),
     path('event/<pk>',event_details),
-    path('user/',participants_list),
-    path('user/<pk>',participant_details),
+    path('participant/',participants_list),
+    path('participant/<pk>',participant_details),
     path('event_user/<pk>',single_event),
     path('register/',registration_view),
     path('login/',obtain_auth_token)
